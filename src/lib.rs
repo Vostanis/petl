d91cf9efab77511713217900349743c75a950ec8
Vostanis <kimonvostanis@gmail.com>
2026-03-20 14:04:48 +0000

    lib documentation added

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,30 @@
+//! # Piped ETL
+//!
+//! Framework for inter-connection data processes, focused around the [`Connections`] struct,
+//! and includes useful [`fs`] functions.
+
 pub mod prelude {
     pub use crate::PostgreSQL;
+    pub use crate::connections::Connections;
     pub use crate::http::extract::HttpExtractExt;
     pub use crate::postgres::{extract::PgExtractExt, load::PgLoadExt};
-    pub use crate::session::Session;
 }
 
-pub use session::Session;
-
+// Re-exports.
+pub use connections::Connections;
 pub use postgres::config::Config as PgPoolConfig;
-
-pub mod postgres;
 
 // Re-export for `PostgreSQL` macro.
 pub use petl_macros::PostgreSQL;
+
+/// Re-export of `tokio_postgres::types::ToSql` (originally from `postgres_types`).
 pub use tokio_postgres::types::ToSql;
-
-pub mod http;
-
-pub mod fs;
-
-/// Focal point of the crate - container for all connections.
-pub mod session;
 
 /// Focal point of the crate - container for all connections.
 pub mod connections;
-pub use connections::Connections;
+
+/// Useful filestore-management functions.
+pub mod fs;
+
+pub mod http;
+pub mod postgres;
